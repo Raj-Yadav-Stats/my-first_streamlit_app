af52e1aae97e020c273a7ee085d58edca7474f29
Raj-Yadav-Stats <yadavraj4610@gmail.com>
2025-02-18 13:49:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ st.set_page_config(
     page_icon="—"
 )
 st.title("Main Page")
+st.header('Gregor Mendel',divider=True)
 st.sidebar.sucess('Pages Above')
 with st.sidebar:
     st.page_link("pages/data.py",label="Home",icon="🏠")
